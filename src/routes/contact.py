30c02c444b831ef31834fb05f90499e644ae1c79
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-07-31 16:02:21 +0200

    feat: add contact information page
    - Add a page where guests can find contact information for the wedding
    couple.
    
    - A link to the contact page can be found on the page footer.

---
+++ src/routes/contact.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from flask import Blueprint, current_app, render_template
+
+bp = Blueprint("contact", __name__)
+
+
+@bp.route("/")
+def contact() -> str:
+    """Render the application's "contact us"" page.
+
+    Returns:
+        str: The rendered HTML for the contact information page.
+    """
+    return render_template(
+        "contact.html",
+        app_name=current_app.config["APP_NAME"],
+        current_year=datetime.now().year,
+    )
