19facb354021bb25e8421686494a3541bb683bf1
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-07-31 13:32:28 +0200

    refact: move 'home' page to routes/ directory

---
+++ src/routes/home.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, current_app, render_template
+
+bp = Blueprint("home", __name__)
+
+
+@bp.route("/home")
+@bp.route("/")
+def home() -> str:
+    """Render the application's landing page.
+
+    Returns:
+        str: The rendered HTML for the landing page.
+    """
+    return render_template("home.html", app_name=current_app.config["APP_NAME"])
