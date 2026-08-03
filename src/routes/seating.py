4fe00a0fa6f545512bf88201cc6e53e3828d7a0f
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-08-03 15:02:09 +0200

    feat: add placeholder seating arrangement page

---
+++ src/routes/seating.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from flask import Blueprint, current_app, render_template
+
+bp = Blueprint("seating", __name__)
+
+
+@bp.route("/")
+def seating() -> str:
+    """Render the application's "seating" page.
+
+    Returns:
+        str: The rendered HTML for the page containing table seating information.
+    """
+    return render_template(
+        "seating.html",
+        app_name=current_app.config["APP_NAME"],
+        current_year=datetime.now().year,
+    )
