a5152f76dcb6f629c39161220088f02d85e4dc62
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-08-21 21:04:35 +0200

    feat: improve error handling of guests endpoint

--- src/api/v1/health.py
+++ src/api/v1/health.py
@@ -3,11 +3,13 @@ api.v1.health - Health check API resource
 -----------------------------------------
 """
 
-from flask import Blueprint
+from flask import Blueprint, Response, jsonify
 
 bp = Blueprint("health", __name__)
 
 
 @bp.get("")
-def health_check() -> tuple[dict[str, str], int]:
-    return {"status": "healthy"}, 200
+def health_check() -> Response:
+    response = jsonify({"status": "healthy"})
+    response.status_code = 200
+    return response
